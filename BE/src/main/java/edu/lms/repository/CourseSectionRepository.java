198da008d5fffcf629ea9df0881f2d808638541e
npt <trangnphe171779@fpt.edu.vn>
2025-10-28 00:21:12 +0700

    modified

--- BE/src/main/java/edu/lms/repository/CourseSectionRepository.java
+++ BE/src/main/java/edu/lms/repository/CourseSectionRepository.java
@@ -11,11 +11,7 @@ import java.util.Optional;
 
 @Repository
 public interface CourseSectionRepository extends JpaRepository<CourseSection, Long> {
-    @Query("SELECT cs FROM CourseSection cs LEFT JOIN FETCH cs.lessons WHERE cs.course.courseID = :courseID")
-    List<CourseSection> findByCourse_CourseID(@Param("courseID") Long courseID);
-    
-    @Query("SELECT cs FROM CourseSection cs LEFT JOIN FETCH cs.lessons l LEFT JOIN FETCH l.resources WHERE cs.sectionID = :sectionId")
-    Optional<CourseSection> findByIdWithLessons(@Param("sectionId") Long sectionId);
+    List<CourseSection> findByCourse_CourseID(Long courseID);
     
     @Query("SELECT cs FROM CourseSection cs WHERE cs.sectionID = :sectionId AND cs.course.tutor.tutorID = :tutorId")
     Optional<CourseSection> findBySectionIdAndTutorId(@Param("sectionId") Long sectionId, @Param("tutorId") Long tutorId);
