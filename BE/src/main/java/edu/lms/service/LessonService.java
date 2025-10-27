1348e1b13ee26a605bc3daef10ec6427ce06ee69
npt <trangnphe171779@fpt.edu.vn>
2025-10-28 04:05:32 +0700

    modified

--- BE/src/main/java/edu/lms/service/LessonService.java
+++ BE/src/main/java/edu/lms/service/LessonService.java
@@ -1,6 +1,5 @@
 package edu.lms.service;
 
-
 import edu.lms.dto.request.LessonRequest;
 import edu.lms.dto.response.LessonResponse;
 
@@ -8,19 +7,20 @@ import java.util.List;
 
 public interface LessonService {
 
-    LessonResponse createLesson(Long sectionId, LessonRequest request, Long tutorId);
+    LessonResponse createLesson(Long sectionID, LessonRequest request, Long tutorId);
 
-    List<LessonResponse> getLessonsBySection(Long sectionId, Long tutorId);
+    List<LessonResponse> getLessonsBySection(Long sectionID, Long tutorId);
 
-    List<LessonResponse> getLessonsBySectionWithFilters(Long sectionId, Long tutorId, String keyword, String sortBy, String order);
+    List<LessonResponse> getLessonsBySectionWithFilters(Long sectionID, Long tutorId, String keyword, String sortBy,
+            String order);
 
     LessonResponse getLessonDetail(Long lessonId, Long tutorId);
 
     LessonResponse updateLesson(Long lessonId, LessonRequest request, Long tutorId);
 
     void deleteLesson(Long lessonId, Long tutorId);
-    
-    Long getTutorIdBySection(Long sectionId);
-    
+
+    Long getTutorIdBySection(Long sectionID);
+
     Long getTutorIdByLesson(Long lessonId);
 }
