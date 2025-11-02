62fb3672584539802b2d71abc3a78e84976ac745
npt <trangnphe171779@fpt.edu.vn>
2025-11-02 23:52:11 +0700

    add weektogenerate

--- BE/src/main/java/edu/lms/dto/response/TutorBookingPlanResponse.java
+++ BE/src/main/java/edu/lms/dto/response/TutorBookingPlanResponse.java
@@ -19,6 +19,8 @@ public class TutorBookingPlanResponse {
     String startHour;
     String endHour;
     String activeDays;
+    Integer weekToGenerate;
     Integer maxLearners;
     String tutorName;
+    Integer numberOfGeneratedSlots;
 }
