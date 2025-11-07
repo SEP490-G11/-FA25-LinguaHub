91ffde2f6ef2b8af0c419401f8c458509ccb2aec
npt <trangnphe171779@fpt.edu.vn>
2025-11-08 01:53:41 +0700

    add tutor detail response

---
+++ BE/src/main/java/edu/lms/dto/response/TutorDetailResponse.java
@@ -0,0 +1,32 @@
+package edu.lms.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TutorDetailResponse {
+    Long tutorId;
+    Long userId;
+    String userName;
+    String userEmail;
+    String avatarURL;
+    String country;
+    String phone;
+    String bio;
+
+    Short experience;
+    String specialization;
+    String teachingLanguage;
+    BigDecimal rating;
+    String status;
+
+    List<TutorCourseResponse> courses;
+}
+
