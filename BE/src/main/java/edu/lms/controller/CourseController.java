b53ec34aaa6ad78225bfe86406df12b4b4949812
ThaiHE1735 <thaibqhe173335@fpt.edu.vn>
2025-11-09 03:22:03 +0700

    UPDATE CODE ABOUT COURSES LiST AND COURSE DETAIL, ADD VIEW FEEDBACK AND RATING, ADD WISH LIST

--- BE/src/main/java/edu/lms/controller/CourseController.java
+++ BE/src/main/java/edu/lms/controller/CourseController.java
@@ -1,12 +1,13 @@
 package edu.lms.controller;
 
 import edu.lms.dto.request.ApiRespond;
+import edu.lms.dto.response.CourseDetailResponse;
 import edu.lms.dto.response.CourseResponse;
-import edu.lms.enums.CourseStatus;
 import edu.lms.service.CourseService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,8 +26,9 @@ public class CourseController {
     @Operation(summary = "Public: Get all Approved courses")
     @GetMapping("/public/approved")
     public ApiRespond<List<CourseResponse>> getAllApprovedPublic() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
         return ApiRespond.<List<CourseResponse>>builder()
-                .result(courseService.getAllApproved())
+                .result(courseService.getAllApproved(email))
                 .build();
     }
 
@@ -34,17 +36,19 @@ public class CourseController {
     @Operation(summary = "Public: Get Approved courses by tutor")
     @GetMapping("/public/approved/{tutorID}")
     public ApiRespond<List<CourseResponse>> getApprovedByTutorPublic(@PathVariable Long tutorID) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
         return ApiRespond.<List<CourseResponse>>builder()
-                .result(courseService.getApprovedByTutor(tutorID))
+                .result(courseService.getApprovedByTutor(tutorID, email))
                 .build();
     }
 
     // Public: xem chi tiết một course (ai cũng xem được)
     @Operation(summary = "Public: Get course detail by ID")
     @GetMapping("/detail/{courseID}")
-    public ApiRespond<CourseResponse> getCourseById(@PathVariable Long courseID) {
-        return ApiRespond.<CourseResponse>builder()
-                .result(courseService.getCourseById(courseID))
+    public ApiRespond<CourseDetailResponse> getCourseById(@PathVariable Long courseID) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return ApiRespond.<CourseDetailResponse>builder()
+                .result(courseService.getCourseById(courseID, email))
                 .build();
     }
 }
