3a1fc99f51a6f0c5a6b7ec3c889a26c0a423f8f1
npt <trangnphe171779@fpt.edu.vn>
2025-11-15 14:01:54 +0700

    add tutor package response

---
+++ BE/src/main/java/edu/lms/dto/response/TutorPackageResponse.java
@@ -0,0 +1,30 @@
+package edu.lms.dto.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TutorPackageResponse {
+    Long packageId;
+    Long tutorId;
+    String name;
+    String description;
+    BigDecimal price;
+    Integer maxSlot;
+    Boolean active;
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
+}
+
+
