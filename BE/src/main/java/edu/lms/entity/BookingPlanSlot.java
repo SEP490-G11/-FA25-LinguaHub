ddbb4f542cae07f9f857101a23c2e6744a7f1119
npt <trangnphe171779@fpt.edu.vn>
2025-11-16 15:43:53 +0700

    modifide booking plan slot

--- BE/src/main/java/edu/lms/entity/BookingPlanSlot.java
+++ BE/src/main/java/edu/lms/entity/BookingPlanSlot.java
@@ -12,23 +12,23 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "Booking_Plan_Slot")
+@Table(name = "booking_plan_slot")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class BookingPlanSlot {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "slot_id")
+    @Column(name = "slotid")
     Long slotID;
 
-    @Column(name = "booking_plan_id", nullable = false)
+    @Column(name = "booking_plan_id")
     Long bookingPlanID;
 
     @Column(name = "tutor_id", nullable = false)
-    Long tutorID; // tutor có thể xem danh sách learner book slot của mình
+    Long tutorID;
 
-    @Column(name = "user_id", nullable = true)
-    Long userID; // learner nào đã book slot
+    @Column(name = "user_id")
+    Long userID;
 
     @Column(name = "start_time", nullable = false)
     LocalDateTime startTime;
@@ -36,23 +36,21 @@ public class BookingPlanSlot {
     @Column(name = "end_time", nullable = false)
     LocalDateTime endTime;
 
-    @Column(name = "payment_id", nullable = true)
-    Long paymentID; // liên kết với bảng Payment
+    @Column(name = "payment_id")
+    Long paymentID;
 
     @Builder.Default
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 10)
+    @Column(nullable = false, columnDefinition = "ENUM('Available','Locked','Paid')")
     SlotStatus status = SlotStatus.Available;
 
-    @Column(name = "locked_at", nullable = true)
+    @Column(name = "locked_at")
     LocalDateTime lockedAt;
 
-    @Column(name = "expires_at", nullable = true)
+    @Column(name = "expires_at")
     LocalDateTime expiresAt; // slot hết hạn nếu chưa thanh toán
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_package_id")
     UserPackage userPackage;
-
-
 }
